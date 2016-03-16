ab193310f01d84ff53a11805b45d75b9aa486de7
Jesse Tipton <me@jessetipton.com>
2016-03-16 03:31:45 -0500

    used URL param

--- iOS/Museek/Museek/APIClient.swift
+++ iOS/Museek/Museek/APIClient.swift
@@ -20,7 +20,7 @@ class APIClient {
         let URL = "" // TODO: This needs to be the URL we're making the request to
         let parameters: [String: AnyObject] = [:] // TODO: (MAYBE) Parameters for the request (Probably a location in some form? Maybe not?)
         let headers: [String: String] = [:] // TODO: (MAYBE) Headers, typically only used for authentication, we probably don't need it for this app
-        Alamofire.request(.GET, "", parameters: parameters, encoding: .URL, headers: headers).responseJSON { response in
+        Alamofire.request(.GET, URL, parameters: parameters, encoding: .URL, headers: headers).responseJSON { response in
             // TODO: Use the `response` object in this closure to convert the JSON blob to an array of Event model objects. If you need help, look at https://github.com/ovenbits/ModelRocket and https://github.com/Alamofire/Alamofire
             completionHandler(events: [])
         }
